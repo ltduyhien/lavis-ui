4b3d03c508c61513c3780006b1c03c5cdd1aeca7
hien <letranduyhien@gmail.com>
2026-03-07 07:32:39 +0200

    chore(frontend): add Jest test infrastructure

---
+++ frontend/jest.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: false,
+        tsconfig: { jsx: 'react-jsx', esModuleInterop: true },
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testMatch: ['**/__tests__/**/*.test.{ts,tsx}', '**/*.test.{ts,tsx}'],
+  modulePathIgnorePatterns: ['<rootDir>/dist', '<rootDir>/node_modules'],
+  watchman: false,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/main.tsx',
+    '!src/**/*.d.ts',
+    '!src/**/index.ts',
+  ],
+};
